61fa1ca4b2ee1d54c5aa581f453d40b496469176
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-11 00:10:38 -0300

    feat(ui): adds grid component

---
+++ src/common/UI/Grid/Container.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 80rem;
+  width: 100%;
+
+  padding-top: 6rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+`
+
+export { Container }
