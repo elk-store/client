24203275237dcf0a1a72230064079565fe63f6f9
Rafael Stein Matsumoto <rafaelmatsumoto43@gmail.com>
2020-05-12 23:04:13 -0300

    feat(ui): add address, order and payment card

---
+++ src/modules/Cards/Address.tsx
@@ -0,0 +1,63 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import DeleteIcon from '@material-ui/icons/Delete'
+import React from 'react'
+
+import { CardProps } from './card.model'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    height: '100%',
+  },
+  title: {
+    fontSize: 14,
+  },
+  deleteButton: {
+    backgroundColor: 'red',
+    color: 'white',
+  },
+  actions: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+})
+
+const Address: React.FC<CardProps> = (props) => {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} gutterBottom>
+          R. Visc. de Taunay, 427 - Centro, Joinville - SC, 89203-005
+        </Typography>
+        {props.default ? <Typography>Default</Typography> : ''}
+      </CardContent>
+      <CardActions className={classes.actions}>
+        <Button variant="contained" size="small" color="primary">
+          Change
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          className={classes.deleteButton}
+          startIcon={<DeleteIcon />}
+        >
+          Delete
+        </Button>
+        <Button variant="contained" size="small" color="primary">
+          Set as default
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Address
---
+++ src/modules/Cards/Order.tsx
@@ -0,0 +1,66 @@
+import {
+  Card,
+  CardContent,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    height: '100%',
+  },
+  title: {
+    fontSize: 14,
+  },
+  actions: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+})
+
+const Order: React.FC = () => {
+  const classes = useStyles()
+  const [activeStep, _setActiveStep] = React.useState(1)
+  const steps = ['Payment confirmed', 'Order shipped', 'Order delivered']
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Order ###.###.###
+        </Typography>
+        <Stepper alternativeLabel activeStep={activeStep}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </CardContent>
+      <CardActions className={classes.actions}>
+        <Button variant="contained" size="small" color="primary">
+          Change
+        </Button>
+        <Button variant="contained" size="small" color="primary">
+          Shippment
+        </Button>
+        <Button variant="contained" size="small" color="primary">
+          More info
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Order
