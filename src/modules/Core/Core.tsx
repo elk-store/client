50708d1a7c64b53939695b180d86c78df3173d56
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-17 20:25:48 -0300

    refactor(core): removes container

--- src/modules/Core/Core.tsx
+++ src/modules/Core/Core.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { Container as BaseContainer } from 'common/UI'
 import Header from 'modules/Header'
-
-const Container = styled(BaseContainer)`
-  padding-top: 1rem;
-`
 
 const Core: React.FC = ({ children }) => {
   return (
     <>
       <Header />
-      <Container>{children}</Container>
+
+      {children}
     </>
   )
 }
