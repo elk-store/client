187e12536be972c2bd9749a48c9cd4671d426eae
Seidel <joao.santos2@fluig.com>
2020-05-18 19:46:15 -0300

    feat(login): add base login page

---
+++ src/modules/Login/Login.tsx
@@ -0,0 +1,60 @@
+import { TextField, FormControl } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import FaceIcon from '@material-ui/icons/Face'
+import React from 'react'
+import SplitPane from 'react-split-pane'
+import styled from 'styled-components'
+
+import { Container as BaseContainer } from 'common/UI'
+
+const Banner = styled.div`
+  background-color: lightblue;
+  padding: 100%;
+`
+
+const Container = styled(BaseContainer)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`
+
+const Row = styled.div`
+  width: auto;
+  text-align: center;
+`
+
+const Logo = styled.img`
+  width: 300px;
+`
+
+const Login: React.FC = () => {
+  return (
+    <SplitPane split="vertical" minSize="30%">
+      <Container>
+        <Row>
+          <Logo src="https://i.imgur.com/ztZsFXg.jpg" />
+          <FormControl margin="dense">
+            <TextField id="username" label="Username" />
+            <TextField id="password" label="Password" />
+
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
+            <Button variant="text">Esqueci minha senha</Button>
+
+            <Button variant="contained" startIcon={<FaceIcon />}>
+              Entrar com Google
+            </Button>
+          </FormControl>
+        </Row>
+      </Container>
+      <Banner />
+    </SplitPane>
+  )
+}
+
+export { Login }
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Login from 'modules/Login'
+
+const Index: React.FC = () => {
+  return <Login />
+}
+
+export default Index
