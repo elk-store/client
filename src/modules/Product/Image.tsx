98b747ea31569539d43c0c2915a7ffc0f318f124
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-19 20:49:42 -0300

    refactor(product): change image component

--- src/modules/Product/Image.tsx
+++ src/modules/Product/Image.tsx
@@ -1,14 +1,25 @@
-import { Paper } from '@material-ui/core'
 import React from 'react'
+import styled from 'styled-components'
+
+const GalleryItem = styled.div`
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.2) !important;
+`
+
+const Item = styled.img`
+  height: auto;
+  max-width: 100%;
+  vertical-align: middle;
+`
 
 const Image: React.FC = () => {
   return (
-    <Paper>
-      <img
+    <GalleryItem>
+      <Item
         src="https://embryo-react.theironnetwork.org/static/media/a-1-a.57b3779d.jpg"
-        alt="Bag"
+        alt="Laptop Backpack"
       />
-    </Paper>
+    </GalleryItem>
   )
 }
 
