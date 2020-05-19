3a1cdfac82be4463c5be7ba7713cec428aac052f
Seidel <joao.santos2@fluig.com>
2020-05-19 16:13:31 -0300

    feat(login): add forget password page and module

---
+++ src/modules/Login/ForgetPassword.tsx
@@ -0,0 +1,40 @@
+import { TextField, Button, Collapse } from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert'
+import React from 'react'
+
+import { Core } from './Core'
+
+const ForgetPassword: React.FC = () => {
+  const [open, setOpen] = React.useState(false)
+
+  const handleClick = () => {
+    setOpen(true)
+  }
+
+  return (
+    <Core>
+      <Collapse in={open}>
+        <Alert variant="outlined" severity="success">
+          A recovery email was sent!
+        </Alert>
+      </Collapse>
+
+      <TextField
+        id="email"
+        label="Recovery email"
+        style={{ marginBottom: '0.5rem' }}
+      />
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ margin: '0.5rem 0' }}
+        onClick={handleClick}
+      >
+        Recover password!
+      </Button>
+    </Core>
+  )
+}
+
+export { ForgetPassword }
---
+++ src/pages/forget-password/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ForgetPassword } from '../../modules/Login/ForgetPassword'
+
+const Index: React.FC = () => {
+  return <ForgetPassword />
+}
+
+export default Index
