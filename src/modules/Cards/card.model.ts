87bb5c4a8f2f4831e5a0462b84bb52524c4c6c26
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-19 15:17:26 -0300

    refactor(profile): change payment tab

--- src/modules/Cards/card.model.ts
+++
@@ -1,3 +0,0 @@
-export type CardProps = {
-  isDefault: boolean
-}
---
+++ src/modules/ProfileContent/Payment/Payment.tsx
@@ -0,0 +1,20 @@
+import { Grid } from '@material-ui/core'
+import React from 'react'
+
+import { PaymentCard } from './PaymentCard'
+
+const PAYMENTS = [1, 2, 3, 4]
+
+const Payment: React.FC = () => {
+  return (
+    <Grid container spacing={2}>
+      {PAYMENTS.map((value) => (
+        <Grid item xs={12} sm={6} key={value}>
+          <PaymentCard />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export { Payment }
