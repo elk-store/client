a56dc44589f9f6f40c0f8abb6449380bded0abed
Seidel <joao.santos2@fluig.com>
2020-05-19 16:13:05 -0300

    feat(login): add register page and module

---
+++ src/modules/Login/Register.tsx
@@ -0,0 +1,40 @@
+import { TextField, Button } from '@material-ui/core'
+import React from 'react'
+
+import { Core } from './Core'
+
+const Register: React.FC = () => {
+  return (
+    <Core>
+      <TextField
+        id="name"
+        label="Personal name"
+        style={{ marginBottom: '0.5rem' }}
+      />
+
+      <TextField
+        id="email"
+        label="Email"
+        type="email"
+        style={{ marginBottom: '0.5rem' }}
+      />
+
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        style={{ marginBottom: '0.5rem' }}
+      />
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ margin: '0.5rem 0' }}
+      >
+        Register
+      </Button>
+    </Core>
+  )
+}
+
+export { Register }
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Register } from '../../modules/Login/Register'
+
+const Index: React.FC = () => {
+  return <Register />
+}
+
+export default Index
