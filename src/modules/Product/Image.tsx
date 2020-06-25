78ffc27580a519f266c7c7c1db1ca0c7cbca06d1
Henrique de Castilhos <henrique@castilhos.dev>
2020-06-25 15:51:01 -0300

    refactor(image): changes to not use an array

--- src/modules/Product/Image.tsx
+++ src/modules/Product/Image.tsx
@@ -3,24 +3,29 @@ import styled from 'styled-components';
 
 const GalleryItem = styled.div`
   box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.14),
-    0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.2) !important;
+    0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.2);
+  padding: 1.25rem;
+  min-height: 12rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Item = styled.img`
   height: auto;
   max-width: 100%;
-  vertical-align: middle;
 `;
 
 type ImageProps = {
-  name: string | undefined;
-  picture: string[] | undefined;
+  name?: string;
+  picture?: string;
 };
 
-const Image: React.FC<ImageProps> = ({ name, picture }) => {
+const Image = ({ name, picture }: ImageProps) => {
   return (
     <GalleryItem>
-      <Item src={picture?.toLocaleString()} alt={name} />
+      <Item src={picture} alt={name} />
     </GalleryItem>
   );
 };
