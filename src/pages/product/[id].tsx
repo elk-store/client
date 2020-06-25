a8021a178f272d685157450b6fe2530006c07334
Henrique de Castilhos <henrique@castilhos.dev>
2020-06-25 15:45:32 -0300

    refactor(product): integrates with api service

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,56 +1,33 @@
 import { Grid } from '@material-ui/core';
-import axios from 'axios';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import styled from 'styled-components';
-import useSWR from 'swr';
 
-import { API_URL } from 'common/constants';
 import { Container } from 'common/UI';
 import Core from 'modules/Core';
 import { Image, Content } from 'modules/Product';
-
-axios.defaults.baseURL = API_URL;
+import { IProduct, ProductService } from 'services/Product';
 
 const Wrapper = styled.div`
   background: var(--color-white);
-  padding: 3rem 0;
 `;
-
-type ContentProps = {
-  sizes: string[];
-  quantity: number;
-  name: string;
-  price: number;
-  code: string;
-  tags: string[];
-  pictures: string[];
-  description: string;
-};
-
-async function fetchData(url: string): Promise<ContentProps> {
-  const response = await axios.get<ContentProps>(url);
-  return response.data;
-}
 
 const Product: React.FC = () => {
   const router = useRouter();
 
-  const { data } = useSWR<ContentProps>(
-    `/products/${router.query.id}`,
-    fetchData
-  );
+  const [product, setProduct] = useState<IProduct>();
+  const [_size, setSize] = useState(['']);
+  const [_quantity, setQuantity] = useState(1);
 
-  const [_size, setSize] = React.useState(['']);
-  const [_quantity, setQuantity] = React.useState(1);
+  useEffect(() => {
+    ProductService.findById(router.query.id as string).then(setProduct);
+  }, [router]);
 
-  const handleSizeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+  const handleSizeChange = (event: ChangeEvent<{ value: unknown }>) => {
     setSize(event.target.value as string[]);
   };
 
-  const handleQuantityChange = (
-    event: React.ChangeEvent<{ value: unknown }>
-  ) => {
+  const handleQuantityChange = (event: ChangeEvent<{ value: unknown }>) => {
     setQuantity(event.target.value as number);
   };
 
@@ -60,17 +37,11 @@ const Product: React.FC = () => {
         <Container>
           <Grid container spacing={2}>
             <Grid item xs={12} md={6}>
-              <Image name={data?.name} picture={data?.pictures} />
+              <Image name={product?.name} picture={product?.pictures?.[0]} />
             </Grid>
             <Grid item xs={12} md={6}>
               <Content
-                sizes={data?.sizes}
-                quantity={data?.quantity}
-                name={data?.name}
-                price={data?.price}
-                code={data?.code}
-                tags={data?.tags}
-                description={data?.description}
+                product={product}
                 handleQuantityChange={handleQuantityChange}
                 handleSizeChange={handleSizeChange}
               />
