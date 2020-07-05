8c44b7734895d3cc68082fa84560b69ba15b7b92
Jessica Amarante <jessica.amarante01@gmail.com>
2020-07-05 15:45:21 -0300

    feat(cart): add cart page

---
+++ src/pages/cart.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Cart: React.FC = () => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody></tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Cart;
